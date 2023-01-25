93dac42059f321d25b3f0f7253014116f2b199ca
hongfeizsy <hongfeizsy@gmail.com>
2023-01-25 11:18:56 +0100

    Load scene with delay.

--- TripleAnimals_v1/Assets/Game/Script/SceneLoader.cs
+++ TripleAnimals_v1/Assets/Game/Script/SceneLoader.cs
@@ -5,9 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] float timeToWait;
+    int currentSceneIndex;
+
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    public void LoadStartScreen() {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void WaitAndLoadNextScene()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(DelayedLoadScene(currentSceneIndex + 1));
+    }
+
+    private IEnumerator DelayedLoadScene(int sceneIndex)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadScene(sceneIndex);
+    }
+
 }
