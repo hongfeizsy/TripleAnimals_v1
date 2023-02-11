bd89204215777278485ee6de13b3c1696cec333b
hongfeizsy <hongfeizsy@gmail.com>
2023-02-11 17:16:16 +0100

    RowProducer spawns cards along different direction

--- TripleAnimals_v1/Assets/Game/Script/CardRowProducer.cs
+++ TripleAnimals_v1/Assets/Game/Script/CardRowProducer.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum Direction {
+    Left, Right, Up, Down
+}
+
 public class CardRowProducer : MonoBehaviour
 {
     [SerializeField] int numberOfCards;
+    [SerializeField] Direction direction;
     float shiftInXAxis;
+    float shiftInYAxis;
     float shiftInZAxis;
 
     private void Awake() 
     {
         shiftInXAxis = 0.1f;
+        shiftInYAxis = 0.1f;
         shiftInZAxis = 1f;
     }
 
@@ -25,7 +33,16 @@ public class CardRowProducer : MonoBehaviour
         int layerNumber = cardIndexes.Count;
         for (int i = 0; i < layerNumber; i++)
         {
-            Card cardObject = Instantiate(cardPrefabs[cardIndexes[i]], transform.position - new Vector3(i * shiftInXAxis, 0, i * shiftInZAxis), transform.rotation);
+            Card cardObject;
+            switch (direction) 
+            {
+                case Direction.Right: cardObject = Instantiate(cardPrefabs[cardIndexes[i]], transform.position - new Vector3(i * shiftInXAxis, 0, i * shiftInZAxis), transform.rotation); break;
+                case Direction.Left: cardObject = Instantiate(cardPrefabs[cardIndexes[i]], transform.position - new Vector3(-i * shiftInXAxis, 0, i * shiftInZAxis), transform.rotation); break;
+                case Direction.Up: cardObject = Instantiate(cardPrefabs[cardIndexes[i]], transform.position - new Vector3(0, i * shiftInYAxis, i * shiftInZAxis), transform.rotation); break;
+                case Direction.Down: cardObject = Instantiate(cardPrefabs[cardIndexes[i]], transform.position - new Vector3(0, -i * shiftInYAxis, i * shiftInZAxis), transform.rotation); break;
+                default: continue;
+            }
+
             cardObject.Origin = CardOrigin.Row;
             cardObject.CardIndex = 0;      // All cards from Row Producer has a zero index.
             cardObject.gameObject.transform.parent = this.transform;
